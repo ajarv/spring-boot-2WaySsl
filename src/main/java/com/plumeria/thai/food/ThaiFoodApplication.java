03b0bcea24c0cf45eb1546f23591d56a6d75d430
Ajar Vashisth <ajarv@hotmail.com>
2018-11-15 14:06:13 -0800

    First

---
+++ src/main/java/com/plumeria/thai/food/ThaiFoodApplication.java
@@ -0,0 +1,12 @@
+package com.plumeria.thai.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaiFoodApplication.class, args);
+	}
+}
